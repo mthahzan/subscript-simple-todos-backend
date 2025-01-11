286265ed484abf7287346671c0028c7e2e586f5f
Thahsan Mohomed <mthahzan.developer@gmail.com>
2025-01-11 19:55:57 +0530

    feat: add fastify plugins
    - Installed and configured CORS plugin for Fastify
    - Installed and configured FormBody plugin for Fastify
    - Installed and configured Helmet plugin for Fastify

--- src/App.ts
+++ src/App.ts
@@ -1,13 +1,19 @@
 import Fastify from 'fastify';
+import fastifyCors from '@fastify/cors';
+import fastifyHelmet from '@fastify/helmet';
+import fastifyFormbody from '@fastify/formbody';
 
 import './config';
 import Environment from './config/Environment';
 
 const startServer = async () => {
   // Create a new Fastify instance
-  const App = Fastify({
-    logger: true,
-  });
+  const App = Fastify({ logger: true });
+
+  // Register fastify-formbody plugin
+  App.register(fastifyCors);
+  App.register(fastifyFormbody);
+  App.register(fastifyHelmet);
 
   // Declare a route
   App.get('/', async function handler(request, reply) {
