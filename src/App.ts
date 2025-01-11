9a83c83d5a5a0c6be7bfaadb92bd171473496ec5
Thahsan Mohomed <mthahzan.developer@gmail.com>
2025-01-11 19:48:35 +0530

    feat: add Fastify
    - Installed fastify
    - Configuerd a basic API endpoint

--- src/App.ts
+++ src/App.ts
@@ -1,3 +1,26 @@
-import './config';
+import Fastify from 'fastify';
 
-console.log('Hello, World!');
+import './config';
+import Environment from './config/Environment';
+
+const startServer = async () => {
+  // Create a new Fastify instance
+  const App = Fastify({
+    logger: true,
+  });
+
+  // Declare a route
+  App.get('/', async function handler(request, reply) {
+    return { hello: 'world' };
+  });
+
+  // Run the server!
+  try {
+    await App.listen({ port: Environment.Api.port });
+  } catch (err) {
+    App.log.error(err);
+    process.exit(1);
+  }
+};
+
+startServer();
